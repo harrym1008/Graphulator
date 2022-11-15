2a18bcefe70ea366abf640e1506d292b6451e4c4
harrym1008 <harrym1010@gmail.com>
2022-11-15 20:08:45 +0000

    Testing in-depth equation parsing

--- testing.py
+++ testing.py
@@ -1,23 +1,14 @@
 import sympy as sp
-from scipy.optimize import fsolve
-from numpy import *
-import time
 
-x, y = sp.symbols("x y")
 
-strEqu = input("Enter equation: ")
-t = time.perf_counter()
 
-sides = strEqu.split("=")
+transformations = (sp.parsing.sympy_parser.standard_transformations,
+                   sp.parsing.sympy_parser.implicit_multiplication_application,
+                   sp.parsing.sympy_parser.convert_xor)
+                   
 
-if len(sides) == 2:
-    lhs, rhs = tuple(sp.sympify(side) for side in sides)
-elif len(sides) == 1:
-    lhs, rhs = y, sp.sympify(sides[0])
-else:
-    raise ValueError("Too many equals signs (at most one)")
+expr = sp.parse_expr(input("Enter:"),
+                  transformations=transformations)
 
-equ = sp.Eq(lhs, rhs)
 
-func_np = sp.lambdify(x, equ, modules=["numpy"])
-solution = fsolve(func_np, 0.5)
+print(str(expr))
