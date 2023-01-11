54f70ed7b4d3a4784bd49a6d7a3bdac6c3338380
harrym1008 <harrym1010@gmail.com>
2023-01-11 21:50:29 +0000

    Testing lambdify, doesn't quite work as I want it

--- testing.py
+++ testing.py
@@ -1,20 +1,40 @@
 import timer, math
 import numpy as np
+import sympy as sp
 
+
+
+
+x, y = sp.symbols("x y")
+
+eq = sp.Eq(sp.sympify("x^2 - y"), 4)
+expr = sp.solve(eq, x)[1]
+
+f = sp.lambdify(x, expr, "numpy")
+a = np.arange(100, 10000, 1)
+
+print(expr)
+
+
+
+data1 = []
+data2 = []
 
 timer.ResetTimer()
-for i in range(1000):
-    eval(f"np.sin(i)")
+for i in range(10000):
+    data1.append(eval(f"np.sqrt(4+i)"))
+
 timer.GetTimeSince("Numpy sin")
-for i in range(1000):
-    eval(f"math.sin(i)")
+for i in range(10000):
+    data2.append(eval(f"math.sqrt(4+i)"))
 
 timer.GetTimeSince("Math sin")
 
+print(f(10.1))
 
 
-narr = np.arange(0, 1000, 1)
-narr2 = np.sin(narr + 2)
+narr = np.arange(0, 10000, 0.9)
+narr2 = f(narr)
 timer.GetTimeSince("numpy array sin")
 
 
